65040731f85a7b24e840835eeec99d34224dbc3c
adam nunez <adam.a.nunez@gmail.com>
2017-04-01 20:03:25 -0500

    Updating Readme, Correcting VCD DLC install issues

--- POL_Viscera_Cleanup_Detail_Steam.sh
+++ POL_Viscera_Cleanup_Detail_Steam.sh
@@ -65,20 +65,22 @@ POL_SetupWindow_message "Click 'Next' once $TITLE has begun to download." "$TITL
 # Install DLC
 if [ "$Install_DLC" = "TRUE" ]
 then
+    cd "$WINEPREFIX/drive_c/$PROGRAMFILES/Steam"
     POL_WINE "steam.exe" steam://install/$STEAMID_DLC
     POL_SetupWindow_message "Click 'Next' once $TITLE_DLC has begun to download." "$TITLE_DLC"
 fi
 
-
 #Install Spin-Off games
 if [ "$Install_Santa" = "TRUE" ]
 then
+    cd "$WINEPREFIX/drive_c/$PROGRAMFILES/Steam"
     POL_Wine "steam.exe" steam://install/$STEAMID_SANTA
     POL_Shortcut "steam.exe" "$TITLE_SANTA" "/http://files.playonlinux.com/resources/setups/$PREFIX/$PREFIX_SANTA-48x48.jpg" "steam://rungameid/$STEAMID_SANTA"
     POL_SetupWindow_message "Click 'Next' once $TITLE_SANTA has begun to download." "$TITLE_SANTA"
 fi
 if [ "$Install_Shadow" = "TRUE" ]
 then
+    cd "$WINEPREFIX/drive_c/$PROGRAMFILES/Steam"
     POL_Wine "steam.exe" steam://install/$STEAMID_SHADOW
     POL_Shortcut "steam.exe" "$TITLE_SHADOW" "http://files.playonlinux.com/resources/setups/$PREFIX/$PREFIX_SHADOW-48x48.jpg" "steam://rungameid/$STEAMID_SHADOW"
     POL_SetupWindow_message "Click 'Next' once $TITLE_SHADOW has begun to download." "$TITLE_SHADOW"
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # POL-Scripts
-Play On Linux scripts I have written 
+Play On Linux scripts I have written
+
+The "icons" and "screenshot" folders are only used for archival here. They are required when submitting scripts to POL.
+
+* Viscera Cleanup Detail - Can be used to install VCD, the DLC "House of Horrors" and the two spin off games "Santa's Rapage" and "Shadow Warrior".
