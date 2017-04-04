3a359a63a885d9ff2e308a1b46956fe82aa2220e
adam nunez <adam.a.nunez@gmail.com>
2017-04-04 08:45:56 -0500

    Script Cleanup, updating readme

--- POL_The_Day_The_World_Broke.sh
+++ POL_The_Day_The_World_Broke.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Date :
+# Date : 2016-04-03
 # Last revision :
 # Wine version used : 2.4
 # Distribution used to test : Linux Mint 18.1 x64
@@ -25,28 +25,19 @@ POL_SetupWindow_presentation "$TITLE" "$EDITOR" "$GAME_URL" "$AUTHOR" "$PREFIX"
 POL_SetupWindow_cdrom
 POL_SetupWindow_check_cdrom "tdtwb2.mpx"
 
-# TmpDir comes back with an extra "/" in it
-POL_System_TmpDir="${POL_System_TmpDir/\/\//\/}"
-
-# Setting prefix path
+# Set prefix, download wine if necessary and create
 POL_Wine_SelectPrefix "$PREFIX"
-
-# Downloading wine if necessary and creating prefix
 POL_Wine_PrefixCreate "$WORKING_WINE_VERSION"
 
 # Config
 Set_OS "winxp"
-#POL_Wine_DirectInput "DirectDrawRenderer" "gdi"
+POL_Wine_DirectInput "Desktops" "1024x768"
 
-winecfg #Manually configure virtual desktop and CD Drive
-
-# Install - Fails right now, issue copying some files
-POL_Wine_WaitBefore "$TITLE"
-POL_Wine "$CDROM/SETUP.EXE" # Need to set up CD-Rom drive for this to work. :(
-
+# Install
+POL_Wine "$CDROM/SETUP.EXE"
 POL_Wine_WaitExit "$TITLE"
 
-POL_Shortcut "tdtwb.exe" "$TITLE"
+POL_Shortcut "tdtwb.exe" "$TITLE" #Error, disc missing :\
 
 POL_SetupWindow_Close
 exit 0
--- POL_Titanic_Adventure_Out_of_Time.sh
+++ POL_Titanic_Adventure_Out_of_Time.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Date :
+# Date : 2016-04-02
 # Last revision :
 # Wine version used : 2.4
 # Distribution used to test : Linux Mint 18.1 x64
@@ -25,9 +25,6 @@ POL_SetupWindow_presentation "$TITLE" "$EDITOR" "$GAME_URL" "$AUTHOR" "$PREFIX"
 POL_SetupWindow_cdrom
 POL_SetupWindow_check_cdrom "DATA/BEDRAD1.TRK"
 
-# TmpDir comes back with an extra "/" in it
-POL_System_TmpDir="${POL_System_TmpDir/\/\//\/}"
-
 # Setting prefix path
 POL_Wine_SelectPrefix "$PREFIX"
 
@@ -37,12 +34,12 @@ POL_Wine_PrefixCreate "$WORKING_WINE_VERSION"
 # Config
 Set_OS "winxp"
 POL_Wine_DirectInput "DirectDrawRenderer" "gdi"
+POL_Wine_DirectInput "Desktops" "1024x768"
 
 #ln -s $CDROM $PREFIX/dosdevices/h: -- Doesn't work, needs to be added in winecfg
-winecfg #Manually configure virtual desktop
+winecfg # Need to set up CD-Rom drive, doesn't automount correctly :\
 
 # Install - Fails right now, issue copying some files
-POL_Wine_WaitBefore "$TITLE"
 POL_Wine "$CDROM/SETUP.EXE" # Need to set up CD-Rom drive for this to work. :(
 
 POL_Wine_WaitExit "$TITLE"
