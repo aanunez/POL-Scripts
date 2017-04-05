a143528e32481ec960a1ee28161c3aa9d2ed549f
adam nunez <adam.a.nunez@gmail.com>
2017-04-05 14:46:51 -0500

    Updating Starcraft script, still not running correctly

--- POL_Starcraft_118_PTR.sh
+++ POL_Starcraft_118_PTR.sh
@@ -4,11 +4,11 @@
 # Wine version used : 2.4
 # Distribution used to test : Linux Mint 18.1 x64
 # Author : Adam Nunez
- 
+
 [ "$PLAYONLINUX" = "" ] && exit 0
 source "$PLAYONLINUX/lib/sources"
- 
-TITLE="Starcraft: Brood War PTR"
+
+TITLE="Starcraft 1.18 PTR"
 PREFIX="Starcraft118PTR"
 WORKING_WINE_VERSION="2.5"
 EDITOR="Blizzard"
@@ -17,7 +17,7 @@ AUTHOR="Adam Nunez"
 
 DOWNLOAD_URL="https://d8un0y0mnd29o.cloudfront.net/public/Starcraft+1.18+PTR+1202.zip"
 DOWNLOAD_MD5="e590dc85f5d140a6214496c95271b88d"
-Install_Path="Starcraft+1.18+PTR+1202.zip"
+ZIP_NAME="Starcraft+1.18+PTR+1202.zip"
 
 # Starting the script - do not exist yet
 POL_GetSetupImages "http://files.playonlinux.com/resources/setups/$PREFIX/top.jpg" "http://files.playonlinux.com/resources/setups/$PREFIX/left.jpg" "$TITLE"
@@ -29,31 +29,37 @@ POL_SetupWindow_presentation "$TITLE" "$EDITOR" "$GAME_URL" "$AUTHOR" "$PREFIX"
 # Set prefix, download wine if necessary and create
 POL_Wine_SelectPrefix "$PREFIX"
 POL_Wine_PrefixCreate "$WORKING_WINE_VERSION"
-cd "$PREFIX/drive_c"
 
 # Get the installer
-POL_SetupWindow_InstallMethod "Download,Local"
-if [ "$INSTALL_METHOD" = "Download" ]; then
-    POL_DOWNLOAD "$DOWNLOAD_URL" "$DOWNLOAD_MD5"
+POL_SetupWindow_InstallMethod "DOWNLOAD,LOCAL"
+if [ "$INSTALL_METHOD" = "DOWNLOAD" ]; then
+    POL_System_TmpCreate "$PREFIX"
+    cd $POL_System_TmpDir
+    #POL_Download doesn't work here, not sure why (maybe https?), it doesn't even log an error
+    #POL_DOWNLOAD "$DOWNLOAD_URL" "$DOWNLOAD_MD5"
+    POL_SetupWindow_wait "Downloading..." "$Title"
+    wget $DOWNLOAD_URL
+    Install_Path="$POL_System_TmpDir/$ZIP_NAME"
+    cd ~
 fi
 if [ "$INSTALL_METHOD" = "LOCAL" ]; then
     POL_SetupWindow_browse "$(eval_gettext 'Please select the zip file to use')" "$TITLE"
     Install_Path="$APP_ANSWER"
 fi
 
-# Unzip/Install - p7zip is posix, should be there
+# Unzip/Install
+POL_SetupWindow_wait "Unpacking Starcraft" "$TITLE"
 check_one "unzip" "unzip"
 POL_SetupWindow_missing
+unzip -d ".PlayOnLinux/wineprefix/$PREFIX/drive_c" $Install_Path
 
-unzip $Install_Path
- 
 POL_Shortcut "StarCraft Launcher.exe" "$TITLE"
 POL_Shortcut "StarEdit.exe" "StarEdit"
 
 # Cleanup the download
-if [ "$INSTALL_METHOD" = "Download" ]; then
-    rm $Install_Path
+if [ "$INSTALL_METHOD" = "DOWNLOAD" ]; then
+    POL_System_TmpDelete
 fi
- 
+
 POL_SetupWindow_Close
 exit
--- README.md
+++ README.md
@@ -3,7 +3,23 @@ Play On Linux scripts I have written
 
 The "icons" and "screenshot" folders are only used for archival here. They are required when submitting scripts to POL. Scripts tagged as "NOT POL Approved" do not have working icons.
 
+**Working**
+
 * Viscera Cleanup Detail - Can be used to install VCD, the DLC "House of Horrors" and the two spin off games "Santa's Rapage" and "Shadow Warrior". [Not POL Approved]
+
+**Installing**
+
+* Starcraft 1.18 PTR - Blizzard is releasing starcraft for free. Its currently still in beta (PTR), but this script will fetch and install it. Currently only StarEdit (map maker) works. [Not POL Approved]
+
+* The Day The World Broke - Disc is not found by wine, I assume this is due to some DRM. [Not POL Approved]
+
+**Not Working**
+
 * Titanic: Adventure Out of Time - Not currently working. Install fails to copy a few files, game fails at launch with error. WineHQ reports this as working 7 years ago. [Not POL Approved]
-* The Day The World Broke - Not currently working. Installs correctly. Disc is not found by wine, I assume this is due to some DRM. [Not POL Approved]
-* Starcraft 1.18 PTR - Blizzard is releasing starcraft for free. Its currently still in beta (PTR), but this script will fetch and install it. Currently on StarEdit (map maker) works. [Not POL Approved]
+
+**In The Works**
+
+* Star Trek Elite Force 1 & 2
+* Star Trek Armada 1 & 2
+* Muppet Treasure Island
+* World of Warcraft 1.12.1 Private Server
